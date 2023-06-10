b5369e7ae4567b2205b79c5d6fddcc2dadcf7c49
chicken-tender <ygm815@naver.com>
2023-06-10 18:48:20 +0900

    :construction: PostRepository 진행중
    참고하셔요!

--- src/main/java/com/kh/backend_finalproject/repository/PostRepository.java
+++ src/main/java/com/kh/backend_finalproject/repository/PostRepository.java
@@ -1,6 +1,10 @@
 package com.kh.backend_finalproject.repository;
+import com.kh.backend_finalproject.constant.RegionStatus;
 import com.kh.backend_finalproject.entitiy.PostTb;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<PostTb, Long> {
+    List<PostTb> findbyRegion(RegionStatus region);
+    List<PostTb> findbyTitleOrDistrictOrPlaceTagOrContentContaining(String keyword);
 }

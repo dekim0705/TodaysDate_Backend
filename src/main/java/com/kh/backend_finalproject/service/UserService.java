efd5343298f7bf48f5c9d35e6b3ae0c8395db8ba
dekim0705 <dekim0705@gmail.com>
2023-06-14 20:18:14 +0900

    :white_check_mark: 마이페이지 - 회원의 모든 게시글/댓글 가져오기 ok

--- src/main/java/com/kh/backend_finalproject/service/UserService.java
+++ src/main/java/com/kh/backend_finalproject/service/UserService.java
@@ -1,16 +1,20 @@
 package com.kh.backend_finalproject.service;
-import com.kh.backend_finalproject.dto.UserDto;
-import com.kh.backend_finalproject.dto.UserProfileDto;
+import com.kh.backend_finalproject.constant.IsPush;
+import com.kh.backend_finalproject.dto.*;
 import com.kh.backend_finalproject.entitiy.PostTb;
 import com.kh.backend_finalproject.dto.UserProfileDto;
+import com.kh.backend_finalproject.entitiy.ReplyTb;
 import com.kh.backend_finalproject.entitiy.UserTb;
 import com.kh.backend_finalproject.repository.PostRepository;
+import com.kh.backend_finalproject.repository.ReplyRepository;
 import com.kh.backend_finalproject.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,17 +25,55 @@ import java.util.Optional;
 public class UserService {
     private final UserRepository userRepository;
     private final PostRepository postRepository;
+    private final ReplyRepository replyRepository;
 
     // ✅ 마이페이지 - 회원 프로필 바 가져오기 (프로필사진, 닉네임, 멤버십 여부, 한 줄 소개, 총 게시글/댓글 수)
     public List<UserProfileDto> getUserProfileInfo(String email) {
         List<UserProfileDto> profileDtos = userRepository.findUserProfileInfo(email);
         return profileDtos;
     }
-    // 마이페이지 - 회원과 매핑된 모든 정보 가져오기
-    public UserTb getUserInfo(Long userId) throws IllegalAccessException {
-        UserTb user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalAccessException("해당 사용자가 없습니다." + userId));
-        return user;
+    // ✅ 마이페이지 - 회원의 모든 게시글 가져오기 (글 번호, 제목, 본문, 조회수, 작성일, 작성자 닉네임)
+    public List<UserDto> getAllUserPosts(String email) {
+        UserTb user = userRepository.findByEmail(email);
+        List<PostTb> posts = user.getPosts();
+        List<UserDto> userDtoList = new ArrayList<>();
+
+        for (PostTb post : posts) {
+            UserDto userDto = new UserDto();
+            userDto.setPostNum(post.getId());
+            userDto.setTitle(post.getTitle());
+            userDto.setContent(post.getContent());
+            userDto.setViewCount(post.getViewCount());
+            userDto.setWriteDate(post.getWriteDate());
+            userDto.setNickname(post.getUser().getNickname());
+
+            userDtoList.add(userDto);
+        }
+        return userDtoList;
     }
 
+    // ✅ 마이페이지 - 회원의 모든 댓글 가져오기 (댓글 번호, 작성자 닉네임, 댓글 본문, 원문 제목, 작성일)
+    public List<UserDto> getAllUserReplies(String email) {
+        UserTb user = userRepository.findByEmail(email);
+        List<ReplyTb> replies = user.getReplies();
+        List<UserDto> userDtoList = new ArrayList<>();
+
+        for (ReplyTb reply : replies) {
+            UserDto userDto = new UserDto();
+            userDto.setReplyNum(reply.getId());
+            userDto.setNickname(reply.getUser().getNickname());
+            userDto.setContent(reply.getContent());
+            userDto.setTitle(reply.getPost().getTitle());
+            userDto.setWriteDate(reply.getWriteDate());
+
+            userDtoList.add(userDto);
+        }
+        return userDtoList;
+    }
+
+    // 마이페이지 - 푸쉬알림 상태 확인
+    public IsPush getUserNotificationStatus(String email) {
+        UserTb user = userRepository.findByEmail(email);
+        return user.getIsPush();
+    }
 }
---
+++ src/main/resources/dummy_test.sql
@@ -0,0 +1,147 @@
+-- USER_TB
+INSERT INTO user_tb (user_num, email, pwd, nickname, user_comment, pf_img, user_region, reg_date, is_push, is_membership, auth_key, is_active)
+VALUES (1, 'user1@naver.com', 'a1234!@#$1', '명란젓코난', '어디든 갈 준비가 되어있습니다.', '기본이미지1', 'SEOUL', '2023-06-01 12:00:00', 'PUSH', 'MEMBERSHIP', 'authKey1', 'ACTIVE');
+
+INSERT INTO user_tb (user_num, email, pwd, nickname, user_comment, pf_img, user_region, reg_date, is_push, is_membership, auth_key, is_active)
+VALUES (2, 'user2@naver.com', 'a1234!@#$1', '꿔바로우많이두개더', '맛집 탐방 위주로 다니고 있습니다.', '기본이미지2', 'BUSAN', '2023-06-05 12:00:00', 'PUSH', 'FREE', 'authKey2', 'ACTIVE');
+
+INSERT INTO user_tb (user_num, email, pwd, nickname, user_comment, pf_img, user_region, reg_date, is_push, is_membership, auth_key, is_active)
+VALUES (3, 'user3@nate.com', 'a1234!@#$1', '이웃집또털어', '핫플 좋아해요.', '기본이미지3', 'SEOUL', '2023-06-07 12:00:00', 'PUSH', 'FREE', 'authKey3', 'ACTIVE');
+
+INSERT INTO user_tb (user_num, email, pwd, nickname, user_comment, pf_img, user_region, reg_date, is_push, is_membership, auth_key, is_active)
+VALUES (4, 'user4@google.com', 'a1234!@#$1', '하리보젤리', '하리보와 함께라면 어떤 데이트라도 좋아.', '기본이미지4', NULL, '2023-06-07 12:00:00', 'PUSH', 'FREE', 'authKey4', 'ACTIVE');
+
+INSERT INTO user_tb (user_num, email, pwd, nickname, user_comment, pf_img, user_region, reg_date, is_push, is_membership, auth_key, is_active)
+VALUES (5, 'user5@naver.com', 'a1234!@#$1', '재롱이', '반려견과 함께 갈 수 있는 데이트 코스 선호해요.', '기본이미지5', 'SEOUL', '2023-06-07 12:00:00', 'PUSH', 'FREE', 'authKey5', 'ACTIVE');
+
+INSERT INTO user_tb (user_num, email, pwd, nickname, user_comment, pf_img, user_region, reg_date, is_push, is_membership, auth_key, is_active)
+VALUES (6, 'user6@google.com', 'a1234!@#$1', '샹궈가좋아', '샹궈집은 어디에나 있죠! 매일 먹고 싶은 샹궈.', '기본이미지6', 'INCHEON', '2023-06-07 12:00:00', 'PUSH', 'FREE', 'authKey6', 'ACTIVE');
+
+INSERT INTO user_tb (user_num, email, pwd, nickname, user_comment, pf_img, user_region, reg_date, is_push, is_membership, auth_key, is_active)
+VALUES (7, 'user7@kakao.com', 'a1234!@#$1', '푸린세스', '판다는 어디에 있나요? 판다 찾아 삼만리', '기본이미지7', 'CHUNGBUK', '2023-06-07 12:00:00', 'NOPUSH', 'FREE', 'authKey7', 'ACTIVE');
+
+INSERT INTO user_tb (user_num, email, pwd, nickname, user_comment, pf_img, user_region, reg_date, is_push, is_membership, auth_key, is_active)
+VALUES (8, 'user8@naver.com', 'a1234!@#$1', '뜨거운아아', '예쁜 카페를 좋아해요! 취미는 카페 투어입니다.', '기본이미지8', 'BUSAN', '2023-06-07 12:00:00', 'PUSH', 'FREE', 'authKey8', 'ACTIVE');
+
+INSERT INTO user_tb (user_num, email, pwd, nickname, user_comment, pf_img, user_region, reg_date, is_push, is_membership, auth_key, is_active)
+VALUES (9, 'user9@google.com', 'a1234!@#$1', '임정환', 'NCT가 누구야? 대단한 사람들이지~', '기본이미지9', 'SEOUL', '2023-06-07 12:00:00', 'PUSH', 'FREE', 'authKey9', 'ACTIVE');
+
+INSERT INTO user_tb (user_num, email, pwd, nickname, user_comment, pf_img, user_region, reg_date, is_push, is_membership, auth_key, is_active)
+VALUES (10, 'user10@kakao.com', 'a1234!@#$1', '룽지', '안녕하냥, 더 많은 츄르를 먹고 시포냥!', '기본이미지10', 'JEOLLANAM', '2023-06-07 12:00:00', 'PUSH', 'MEMBERSHIP', 'authKey10', 'ACTIVE');
+
+INSERT INTO user_tb (user_num, email, pwd, nickname, user_comment, pf_img, user_region, reg_date, is_push, is_membership, auth_key, is_active)
+VALUES (11, 'user11@google.com', 'a1234!@#$1', '겨울잠자는중', '겨울잠에서 깨어나면 데이트만 할꼬얌', '기본이미지11', 'GANGWON', '2023-06-07 12:00:00', 'PUSH', 'FREE', 'authKey11', 'ACTIVE');
+
+INSERT INTO user_tb (user_num, email, pwd, nickname, user_comment, pf_img, user_region, reg_date, is_push, is_membership, auth_key, is_active)
+VALUES (12, 'user12@naver.com', 'a1234!@#$1', '양파쿵야', '쿵야와 함께하는 데이트 코스 구경하세요!', '기본이미지12', 'JEJU', '2023-06-07 12:00:00', 'PUSH', 'FREE', 'authKey12', 'ACTIVE');
+
+INSERT INTO user_tb (user_num, email, pwd, nickname, user_comment, pf_img, user_region, reg_date, is_push, is_membership, auth_key, is_active)
+VALUES (13, 'user13@google.com', 'a1234!@#$1', '화가많은사람', '화 풀리는 코스 위주로 돌아다닙니다! ', '기본이미지13', 'JEJU', '2023-06-07 12:00:00', 'PUSH', 'MEMBERSHIP', 'authKey13', 'ACTIVE');
+
+INSERT INTO user_tb (user_num, email, pwd, nickname, user_comment, pf_img, user_region, reg_date, is_push, is_membership, auth_key, is_active)
+VALUES (14, 'user14@kakao.com', 'a1234!@#$1', '임실피즈치자', '치즈 요리를 판매하는 음식점을 좋아해요!.', '기본이미지14', NULL, '2023-06-07 12:00:00', 'PUSH', 'FREE', 'authKey14', 'ACTIVE');
+
+INSERT INTO user_tb (user_num, email, pwd, nickname, user_comment, pf_img, user_region, reg_date, is_push, is_membership, auth_key, is_active)
+VALUES (15, 'user15@naver.com', 'a1234!@#$1', '짱구는못말려', '천방지축 어리둥절 빙글빙글 돌아가는 하루.', '기본이미지15', NULL, '2023-06-07 12:00:00', 'PUSH', 'FREE', 'authKey15', 'ACTIVE');
+
+-- POST_TB
+-- 1번 게시글
+INSERT INTO post_tb (post_num, title, region, course, theme, district, img_url, view_count, write_date, content, user_num)
+VALUES (1, '서울의 명소: 경복궁', 'SEOUL', '당일 치기', '힐링 코스', '서울시 종로구', '이미지1', 100, '2023-06-02 00:00:00', '경복궁은 조선 시대 왕궁입니다. 임금의 거처였던 곳이며, 정부의 중심지였습니다.', 1);
+
+INSERT INTO post_tb_comment (post_tb_post_num, comment)
+VALUES (1, '대중교통이 편해요'), (1, '사진찍기 좋아요'), (1, '야경이 이뻐요');
+
+INSERT INTO post_tb_place_tag (post_tb_post_num, place_tag)
+VALUES (1, '경복궁'), (1, '창덕궁'), (1, '어라운드 시소');
+
+INSERT INTO pin_tb (pin_num, latitude, longitude, route_num, post_num)
+VALUES (1, 3.3434, 34.2323, 1, 1);
+
+INSERT INTO push_tb (push_num, send_date, post_num_fk, user_num_fk)
+VALUES (1, '2023-06-02 00:01:00', 1, 3);
+
+INSERT INTO reply_tb (reply_num, content, write_date, post_num, user_num)
+VALUES (1, '좋은 코스 공유 감사합니다.', '2023-06-03 00:00:00', 1, 3);
+
+-- 2번 게시글
+INSERT INTO post_tb (post_num, title, region, course, theme, district, view_count, write_date, content, user_num)
+VALUES (2, '부산 해운대 바다 여행', 'BUSAN', '1박 2일', '맛집 탐방', '부산시 해운대구', 200, '2023-06-06 11:00:00', '코엑스는 한국 최대의 쇼핑과 엔터테인먼트 복합 시설입니다. 많은 가게와 레스토랑이 있습니다.', 2);
+
+INSERT INTO post_tb_comment (post_tb_post_num, comment)
+VALUES (2, '주차가 힘들어요'), (2, '사람이 많아요'), (2, '바가지 조심해요');
+
+INSERT INTO post_tb_place_tag (post_tb_post_num, place_tag)
+VALUES (2, '서면 회센터'), (2, '남천정'), (2, '쿠키인가곰');
+
+-- ALTER TABLE post_tb_comment add column id int auto_increment primary key;
+-- ALTER TABLE post_tb_place_tag add column id int auto_increment primary key;
+-- ALTER TABLE post_tb_img_url add column id int auto_increment primary key;
+
+-- FOLDER_TB
+INSERT INTO folder_tb (folder_num, name, user_id)
+VALUES (1, '북마크1-1', 1);
+INSERT INTO folder_tb (folder_num, name, user_id)
+VALUES (2, '북마크1-2', 1);
+INSERT INTO folder_tb (folder_num, name, user_id)
+VALUES (3, '북마크1-3', 1);
+INSERT INTO folder_tb (folder_num, name, user_id)
+VALUES (4, '북마크2-1', 2);
+INSERT INTO folder_tb (folder_num, name, user_id)
+VALUES (5, '북마크2-2', 2);
+INSERT INTO folder_tb (folder_num, name, user_id)
+VALUES (6, '북마크2-3', 2);
+INSERT INTO folder_tb (folder_num, name, user_id)
+VALUES (7, '북마크1-4', 1);
+INSERT INTO folder_tb (folder_num, name, user_id)
+VALUES (8, '북마크2-4', 2);
+INSERT INTO folder_tb (folder_num, name, user_id)
+VALUES (9, '북마크2-5', 2);
+INSERT INTO folder_tb (folder_num, name, user_id)
+VALUES (10, '북마크1-5', 1);;
+
+-- BOOKMARK_TB
+INSERT INTO bookmark_tb (bookmark_num, folder_id, post_id)
+VALUES (1, 1, 1);
+INSERT INTO bookmark_tb (bookmark_num, folder_id, post_id)
+VALUES (2, 1, 2);
+--INSERT INTO bookmark_tb (bookmark_num, folder_id, post_id)
+--VALUES (3, 1, 3);
+INSERT INTO bookmark_tb (bookmark_num, folder_id, post_id)
+VALUES (4, 2, 1);
+--INSERT INTO bookmark_tb (bookmark_num, folder_id, post_id)
+--VALUES (5, 2, 3);
+INSERT INTO bookmark_tb (bookmark_num, folder_id, post_id)
+VALUES (6, 4, 2);
+--INSERT INTO bookmark_tb (bookmark_num, folder_id, post_id)
+--VALUES (7, 4, 3);
+INSERT INTO bookmark_tb (bookmark_num, folder_id, post_id)
+VALUES (8, 5, 1);
+INSERT INTO bookmark_tb (bookmark_num, folder_id, post_id)
+VALUES (9, 5, 2);
+--INSERT INTO bookmark_tb (bookmark_num, folder_id, post_id)
+--VALUES (10, 5, 3);
+
+-- CHATBOT_TB
+INSERT INTO chatbot_tb (inquiry_num, inquiry_content, user_num, inquiry_date, inquiry_status, email)
+VALUES (1, '문의합니다!! 안녕하세요 ~~~~ 이거 어떻게하는거죵???????? 너무어렵네요.. 관리자님.. 도와주세요 ㅜ-ㅜ', 1, '2023-06-06 11:00:00', '완료', 'user1@naver.com');
+
+INSERT INTO chatbot_tb (inquiry_num, inquiry_content, user_num, inquiry_date, inquiry_status, email)
+VALUES (2, '안녕하세요. ㅇㅇㅇㅇ에 대해 문의드립니다. 블라블라블라', 2, '2023-06-06 11:00:00', '완료', 'user4@naver.com');
+
+INSERT INTO chatbot_tb (inquiry_num, inquiry_content, user_num, inquiry_date, inquiry_status, email)
+VALUES (3, '질문합니다!! 이거 제가 이해한게 맞나요?!!!!', 3, '2023-06-06 12:00:00', '대기', 'user6@naver.com');
+
+--INSERT INTO chatbot_tb (inquiry_num, inquiry_content, user_num, inquiry_date, inquiry_status, email)
+--VALUES (4, '문의합니다!! 안녕하세요 ~~~~ 이렇게 하려면 어떻게 해야하죠? 관리자님.. 도와주세요 ㅜ-ㅜ', 4, '2023-06-06 12:30:00', '대기', 'user9@naver.com');
+--
+--INSERT INTO chatbot_tb (inquiry_num, inquiry_content, user_num, inquiry_date, inquiry_status, email)
+--VALUES (5, '안녕하세요 ~ 문의드립니다~ ', 5, '2023-06-06 12:30:00', '대기', 'user11@naver.com');
+
+-- REPORT_TB
+INSERT INTO report_tb (report_num, content, report_date, reported_fk, reporter_fk)
+VALUES (1, '신고합니다... 저한테 욕했어요 ㅡㅡ','2023-06-06 12:30:00', 1,2);
+INSERT INTO report_tb (report_num, content, report_date, reported_fk, reporter_fk)
+VALUES (2, '이사람 신고합니다. 광고글만 올려요','2023-06-06 12:40:00', 3,1);
+--INSERT INTO report_tb (report_num, content, report_date, reported_fk, reporter_fk)
+--VALUES (3, '신고합니다...','2023-06-06 12:50:00', '이웃집또털어','꿔바로우많이두개더');

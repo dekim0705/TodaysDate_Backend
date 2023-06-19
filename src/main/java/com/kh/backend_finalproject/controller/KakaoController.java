a6f0d29ca17ba830df6e17b3f8d1636bec1b4537
chicken-tender <ygm815@naver.com>
2023-06-19 14:16:29 +0900

    :recycle: 카카오 관련 컨트롤러 간소화

--- src/main/java/com/kh/backend_finalproject/controller/KakaoController.java
+++ src/main/java/com/kh/backend_finalproject/controller/KakaoController.java
@@ -1,5 +1,6 @@
 package com.kh.backend_finalproject.controller;
 
+import com.kh.backend_finalproject.service.KakaoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -18,63 +19,17 @@ import java.util.Map;
 @RequestMapping("/kakao")
 @RequiredArgsConstructor
 public class KakaoController {
-    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
-    private String clientId;
+    private final KakaoService kakaoService;
 
-    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
-    private String clientSecret;
-
-    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
-    private String redirectUri;
-
-    @Value("${spring.security.oauth2.client.provider.kakao.token-uri}")
-    private String tokenUri;
-
-    // 🔑카카오에게 인가 코드 전달하여 토큰 요청(Access, Refresh)
     @PostMapping("")
     public ResponseEntity<?> kakaoLogin(@RequestBody Map<String, String> kakaoAuth) {
-        RestTemplate restTemplate = new RestTemplate();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "authorization_code");
-        params.add("client_id", clientId);
-        params.add("client_secret", clientSecret);
-        params.add("redirect_uri", redirectUri);
-        params.add("code", kakaoAuth.get("authorizationCode"));
-
-        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
-        ResponseEntity<String> response = restTemplate.exchange(
-                tokenUri,
-                HttpMethod.POST,
-                kakaoTokenRequest,
-                String.class
-        );
-        System.out.println("🔑" + response.getBody());
-        return ResponseEntity.ok(response.getBody());
+        String token = kakaoService.requestKakaoToken(kakaoAuth);
+        return ResponseEntity.ok(token);
     }
-
-    // 🔑받은 토큰을 카카오에게 전달하여 사용자 정보 요청
-    @Value("${spring.security.oauth2.client.provider.kakao.user-info-uri}")
-    private String userInfoUri;
 
     @PostMapping("/userinfo")
     public ResponseEntity<?> getUserInfo(@RequestBody String token) {
-        RestTemplate restTemplate = new RestTemplate();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + token);
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(
-                userInfoUri,
-                HttpMethod.GET,
-                entity,
-                String.class
-        );
-        System.out.println("😊 : " + response.getBody());
-        return ResponseEntity.ok(response.getBody());
+        String userInfo = kakaoService.requestKakaoUserInfo(token);
+        return ResponseEntity.ok(userInfo);
     }
 }
---
+++ src/main/java/com/kh/backend_finalproject/service/KakaoService.java
@@ -0,0 +1,76 @@
+package com.kh.backend_finalproject.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
+@Transactional
+@Slf4j
+@RequiredArgsConstructor
+public class KakaoService {
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
+    private String clientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    private String redirectUri;
+
+    @Value("${spring.security.oauth2.client.provider.kakao.token-uri}")
+    private String tokenUri;
+
+    @Value("${spring.security.oauth2.client.provider.kakao.user-info-uri}")
+    private String userInfoUri;
+
+    public String requestKakaoToken(Map<String, String> kakaoAuth) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", clientId);
+        params.add("client_secret", clientSecret);
+        params.add("redirect_uri", redirectUri);
+        params.add("code", kakaoAuth.get("authorizationCode"));
+
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+        ResponseEntity<String> response = restTemplate.exchange(
+                tokenUri,
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+        return response.getBody();
+    }
+
+    public String requestKakaoUserInfo(String token) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token);
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                userInfoUri,
+                HttpMethod.GET,
+                entity,
+                String.class
+        );
+        return response.getBody();
+    }
+}

6f6d70282908cf6cf24df24bc1af919b051867ea
dekim0705 <dekim0705@gmail.com>
2023-07-04 23:48:19 +0900

    :beers: JoinController에 @RestController 다시 추가

--- src/main/java/com/kh/backend_finalproject/controller/JoinController.java
+++ src/main/java/com/kh/backend_finalproject/controller/JoinController.java
@@ -8,13 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-@Controller
-//@RestController
+
+@RestController
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/join")
